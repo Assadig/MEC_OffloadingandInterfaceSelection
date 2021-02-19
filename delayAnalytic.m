c95adedf896bbcbcab6aa3c76703918411c060c1
RushabhaB <f20170220@pilani.bits-pilani.ac.in>
2021-02-19 11:34:31 +0530

    Added comments for each lines and docstring

--- delayAnalytic.m
+++ delayAnalytic.m
@@ -1,14 +1,27 @@
 function [E_delay] = delayAnalytic(k,N,beta,gamma,Tcoll,Tsucc,CWmin,CWmax)
-    E_delay =0;
-    CW = (2.^(0:k))*CWmin;
-    CW(CW>CWmax) = CWmax;
-    b_mean = ((CW+1).*(CW+2))./(2*CW);
-    Pa = 1-((1-beta)^(N-1));
-    Ps = ((N-1)*beta*(1-beta)^(N-2))/Pa;
+    % delayAnalytic : Returns the average delay value experienced by a user
+    % Parameters :
+    %   k : Maximum number of attempts allowed for a pacjet by a user
+    %   N : Number of contending users
+    %   beta : transmission probability
+    %   gamma : Conditional collision probability
+    %   Tcoll : Time the channel is occupied during a collision
+    %   Tsucc : Time the channel is occupied during successfull tr
+    %   CWmin : Minimum contention window size
+    %   CWmax : Maximum contention window size
+    % Return Variables :
+    %   E_Delay = Average Delay for an user in the above system
     
-    Trc = (1-beta)^(N-1) + Pa*Ps*(Tsucc) + Pa*(1-Ps)*(Tcoll);
-    for i=0:k
-        E_delay = E_delay + (sum(b_mean(1:i+1))*Trc + i*(Tcoll) + (Tsucc))*(gamma^i)*(1-gamma);
+    E_delay =0; % Initialising the average delay variable for a selected user
+    CW = (2.^(0:k))*CWmin;  % Creating the CW vector for all the k attempts
+    CW(CW>CWmax) = CWmax; % Capping the maximum valye at CWmax
+    b_mean = ((CW+1).*(CW+2))./(2*CW); % Initialising the mean value vector
+    Pa = 1-((1-beta)^(N-1)); % Probability of attemoting to trasmit for N-1 users
+    Ps = ((N-1)*beta*(1-beta)^(N-2))/Pa; % Successfull tr prob for the N-1 users
+    
+    Trc = (1-beta)^(N-1) + Pa*Ps*(Tsucc) + Pa*(1-Ps)*(Tcoll); % Renewal time for a particular selected user
+    for i=0:k % For all the attempts
+        E_delay = E_delay + (sum(b_mean(1:i+1))*Trc + i*(Tcoll) + (Tsucc))*(gamma^i)*(1-gamma);  % Delay expression 
     end
     
 end
