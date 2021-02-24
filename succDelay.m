7bdedfe4eceb021c5b24bed7b97b955ff6ab8afd
RushabhaB <57673194+RushabhaB@users.noreply.github.com>
2021-02-24 19:18:28 +0530

    Working code for N=30
    Need to test it for higher values of N. Added code to break the
    succession time into chunks between two discarded packets. Took the
    difference in between these and then took the mean

--- succDelay.m
+++ succDelay.m
@@ -1,10 +1,17 @@
-function E_delaysim = succDelay(succTime)
+function E_delaysim = succDelay(succTime,discardTime)
 
 N = size(succTime,2);
 E_delaysim = zeros(1,N);
 for i =1:N
     x = succTime(:,i);
     x=x(x~=0);
-    E_delaysim(i) = mean(diff(x));
+    dic1 = discardTime(:,i);
+    dic1 = dic1(dic1~=0);
+    y=[];
+    for h = 1:length(dic1)-1
+        l = x(x(x<dic1(h+1))>dic1(h));
+        y = [y; diff(l)];
+    end
+    E_delaysim(i) = mean(y);
 end
 end
