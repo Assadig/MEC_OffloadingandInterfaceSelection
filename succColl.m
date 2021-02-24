c194703054e8034854538bd5f223cbf893cf3edd
RushabhaB <57673194+RushabhaB@users.noreply.github.com>
2021-02-24 16:02:47 +0530

    Added code for recording discardTime
    Made sure that the nSuccColl is reset inside when it reaches the value
    of k-1 then and there. Added code for recording the discardFlag as well

--- succColl.m
+++ succColl.m
@@ -1,18 +1,23 @@
-function nSuccColl = succColl(k,attemptTransmit,i,nSuccColl)
+function [nSuccColl,CW,discardTime,d,discardFlag] = succColl(k,attemptTransmit,i,nSuccColl,CW,CWmin,discardTime,d,discardFlag)
     currColl = attemptTransmit(i,:);
     userColl = find(currColl);
     for j = userColl
         prevInd = flip(find(attemptTransmit(1:i,j)));
         if(length(prevInd)>1)
             collFlag = (sum(attemptTransmit(prevInd(2),:))>1);
-            if(nSuccColl(j) < (k-1))
-                if(collFlag == 1)
+            if(collFlag == 1)
+                if(nSuccColl(j) < (k-1))
                     nSuccColl(j) = nSuccColl(j) +1;
-                else
-                    nSuccColl(j) = 0;
+                    if(nSuccColl(j) == (k-1))
+                        nSuccColl(j) = 0;
+                        CW(j) = CWmin;
+                        d=d+1;
+                        discardTime(d,j) = i; 
+                        discardFlag(j) = 1;
+                    end
                 end
             else
-                nSuccColl(j) = 0;
+                nSuccColl(j) = 0;     
             end
         else
             nSuccColl(j)=0;
