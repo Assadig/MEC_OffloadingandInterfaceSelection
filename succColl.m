3ac7857d47490352b23564691ed5e82c6b773d36
RushabhaB <57673194+RushabhaB@users.noreply.github.com>
2021-02-25 19:23:20 +0530

    Added code to implement the discarded packet time
    Recording of when the packet is discarded is being done as well as
    changing the CW to CWmin when nSuccColl=k-1 is being done in the
    function itself. Added a discardFlag as well for \beta calc

--- succColl.m
+++ succColl.m
@@ -1,14 +1,19 @@
-function nSuccColl = succColl(k,attemptTransmit,nSuccColl,succFlag)
+function [nSuccColl,CW,discardTime,d,discardFlag] = succColl(k,attemptTransmit,nSuccColl,succFlag,i,CW,CWmin,discardTime,d,discardFlag)
     userColl = find(attemptTransmit);
     for j = userColl
         if(succFlag(j) == 0)
-            if(nSuccColl(j) < k)
-                nSuccColl(j) = nSuccColl(j) + 1;
-            else
-                nSuccColl(j) = 0;
+            if(nSuccColl(j) < (k-1))
+                nSuccColl(j) = nSuccColl(j) +1;
+                if(nSuccColl(j) == (k-1))
+                    nSuccColl(j) = 0;
+                    CW(j) = CWmin;
+                    d=d+1;
+                    discardTime(d,j) = i; 
+                    discardFlag(j) = 1;
+                end
             end
         else
-            nSuccColl(j) = 0;
+            nSuccColl(j) = 0;     
         end
     end
 end
