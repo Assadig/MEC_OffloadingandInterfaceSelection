95f71d8aaf0d912550419bd38bd38678941565b7
RushabhaB <57673194+RushabhaB@users.noreply.github.com>
2021-02-25 19:15:53 +0530

    Fixed code to build the diff array correctly
    In the previous implementation the values below dic1(1) and above
    dic1(end) would not be considered, I changed that as well as added
    correct calculation if dic1 is []

--- succDelay.m
+++ succDelay.m
@@ -1,10 +1,27 @@
-function E_delaysim = succDelay(succTime)
+function E_delaysim = succDelay(succTime,discardTime)
 
 N = size(succTime,2);
 E_delaysim = zeros(1,N);
 for i =1:N
     x = succTime(:,i);
     x=x(x~=0);
-    E_delaysim(i) = mean(diff(x));
+    dic1 = discardTime(:,i);
+    dic1 = dic1(dic1~=0);
+    y=[];
+    if(~isempty(dic1))
+        for h = 1:length(dic1)
+            if(h ==1)
+                l=x(x<dic1(h));
+            elseif(h== length(dic1))
+                l=x(x>dic1(h));
+            else
+                l = x(x(x<dic1(h+1))>dic1(h));
+            end
+            y = [y; diff(l)];
+        end
+    else
+        y=diff(x);
+    end
+    E_delaysim(i) = mean(y);
 end
 end
