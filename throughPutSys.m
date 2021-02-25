5a1d87df6054da5d9e297e0010089689673e5f63
RushabhaB <57673194+RushabhaB@users.noreply.github.com>
2021-02-25 19:11:10 +0530

    Changed the denomnator i.e. E[SlotTime]
    Previously i had (1-Pa)*1 for the slot time part, however after seeing
    Prof.Anurags paper i see that it shouldd be 1. By making that change the
    sim and analysis match

--- throughPutSys.m
+++ throughPutSys.m
@@ -3,5 +3,5 @@ function S = throughPutSys(N,beta,tPacket,tSucc,tColl)
     Pa = 1-((1-beta)^(N));
     Ps = ((N)*beta*(1-beta)^(N-1))/Pa;
     
-    S = (Ps*Pa*tPacket)/((1-Pa) + Ps*Pa*tSucc + Pa*(1-Ps)*tColl);
+    S = (Ps*Pa*tPacket)/(1 + Ps*Pa*tSucc + Pa*(1-Ps)*tColl);
 end
